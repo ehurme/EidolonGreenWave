b6a61661bf0058e898ae50b40b41ff98b3ab45c9
Edward Hurme <edwardhurme@gmail.com>
2021-12-21 11:57:21 +0100

    paths to lines

--- R/example_phen_extraction_plot.R
+++ R/example_phen_extraction_plot.R
@@ -1,4 +1,7 @@
 # example phenology extraction plot
+library(pacman)
+p_load(ggplot2, lubridate, ggpubr)
+
 getwd()
 source("./src/IRG_functions.R")
 
@@ -53,34 +56,70 @@ example_peaks = data.frame(time = x, EVI = fit$y, IRG = fit.$y11,
 colors <- c("EVI" = "#009E73", "IRG" = "#D55E00", "Prp" = "lightblue", "IRP" = "turquoise", "Count" = "lightgray")
 gg_peaks <- 
   ggplot(example_peaks, aes(x = time))+
-    geom_path(aes(y = EVI, col = "EVI"), size = 1)+
-    geom_path(aes(y = IRG, col = "IRG"), size = 1)+
+    geom_line(aes(y = EVI, col = "EVI"), size = 1, linetype = 5)+
+    geom_line(aes(y = IRG, col = "IRG"), size = 1, linetype = 6)+
     geom_col(aes(y = precip, col = "Prp"), fill = "lightblue", alpha = 0.1)+
-  geom_path(aes(y = IRP, col = "IRP"), size = 1)+
+  geom_line(aes(y = IRP, col = "IRP"), size = 1, linetype = 7)+
     geom_ribbon(aes(ymin = 0, ymax = bats, col = "Count"), alpha = 0.1)+
-    geom_path(aes(y = bats), col = 1)+
+    geom_line(aes(y = bats), col = 1, linetype = 2, size = 1)+
     #scale_color_manual(values=cbPalette)+
     theme_classic()+
-    geom_hline(yintercept = 0, col = "gray")+
-    geom_vline(xintercept = example_peaks$time[which.max(example_peaks$EVI)], 
-               linetype = "dashed", col = "darkgreen", size = 1, alpha = 0.5)+
-    geom_vline(xintercept = example_peaks$time[which.max(example_peaks$IRG)], 
-               linetype = "dashed", col = "darkred", size = 1, alpha = 0.5)+
-    geom_vline(xintercept = example_peaks$time[which.max(example_peaks$precip)], 
-             linetype = "dashed", col = "lightblue", size = 1)+
-    geom_vline(xintercept = example_peaks$time[which.max(example_peaks$bats)], 
-             linetype = "dashed", col = 1, size = 1, , alpha = 0.5)+  
-    geom_vline(xintercept = example_peaks$time[which.max(example_peaks$IRP)], 
-             linetype = "dashed", col = "turquoise", size = 1, alpha = 0.5)+  
-    scale_x_date(date_labels = "%b") +
-    labs(color = "", ylab = "value")+
-    scale_color_manual(values = colors)+
-    theme(text = element_text(size = 15), 
+  # Count  
+  geom_hline(yintercept = 0, col = "gray")+
+  # EVI  
+  geom_vline(xintercept = example_peaks$time[which.max(example_peaks$EVI)], 
+               linetype = 5, col = "darkgreen", size = 1, alpha = 0.5, show.legend = TRUE)+
+  # IRG  
+  geom_vline(xintercept = example_peaks$time[which.max(example_peaks$IRG)], 
+               linetype = 6, col = "darkred", size = 1, alpha = 0.5)+
+  # Precip  
+  geom_vline(xintercept = example_peaks$time[which.max(example_peaks$precip)], 
+             linetype = 8, col = "lightblue", size = 1)+
+  # Count  
+  geom_vline(xintercept = example_peaks$time[which.max(example_peaks$bats)], 
+             linetype = 1, col = 1, size = 1, , alpha = 0.5)+  
+  # IRP  
+  geom_vline(xintercept = example_peaks$time[which.max(example_peaks$IRP)], 
+             linetype = 9, col = "turquoise", size = 1, alpha = 0.5)+  
+    
+  scale_x_date(date_labels = "%b") +
+    
+  labs(color = "", ylab = "value")+ylab("value")+
+    
+  scale_color_manual(values = colors, labels = names(colors))+
+  scale_linetype_manual(values = c(1, 2,3,4,5),
+                        labels = c("EVI", "IRG", "Prp", "IRP", "Count"))+
+    
+  theme(text = element_text(size = 20), 
           # axis.text.x = element_text(angle = 30, vjust = 1, hjust=1),
           legend.position = "top",
           legend.text=element_text(size=12))+
   guides(col=guide_legend(nrow=2,byrow=TRUE))
 gg_peaks
+
+########################
+colors <- c("EVI" = "#009E73", "IRG" = "#D55E00", "Prp" = "lightblue", "IRP" = "turquoise", "Count" = "lightgray")
+example_peaks_mlt <- reshape2::melt(example_peaks, id.vars = c("time"))
+ggplot(example_peaks_mlt, aes(x = time, y = value))+
+  geom_ribbon(data = example_peaks_mlt[example_peaks_mlt$variable == "bats",], 
+              aes(ymin = 0, ymax = value, x = time,
+                  col = "Count"), alpha = 0.1, inherit.aes = FALSE)+
+  # geom_col(data = example_peaks_mlt[example_peaks_mlt$variable == "precip",],
+  #          aes(y = precip, x = time, col = "Prp"), fill = "lightblue", alpha = 0.1,
+  #          inherit.aes = FALSE)+
+  geom_line(aes(col = variable, 
+                linetype = variable),size = 1)+
+  theme_classic()+
+  # Count  
+  geom_hline(yintercept = 0, col = "gray")+
+  theme(text = element_text(size = 20), 
+      # axis.text.x = element_text(angle = 30, vjust = 1, hjust=1),
+      legend.position = "top",
+      legend.text=element_text(size=12))+
+  scale_color_manual(values = colors, labels = names(colors), name = "")+
+  scale_linetype_manual(name="", values = 2:6, labels = names(colors))+
+  guides(col=guide_legend(nrow=2,byrow=TRUE))
+
 
 #### ggplot
 x <- seq.Date(as.Date("2010-01-01"), as.Date("2010-12-01"), by = "week")
@@ -135,14 +174,15 @@ example_EVI$entropy <- factor(c(rep("high", nrow(example_EVI)/3),
                          rep("low", nrow(example_EVI)/3)), 
                          levels = c("high", "med","low"))
 gg_entropy <-               
-  ggplot(example_EVI, aes(x, rescale(EVI), col = entropy, group = ID))+
+  ggplot(example_EVI, aes(x, rescale(EVI), col = entropy, group = ID, 
+                          linetype = entropy))+
     ylab("EVI")+
     xlab("time")+
-    geom_path(size = 1)+
+    geom_line(size = 1)+
     theme_classic()+
     scale_color_manual(values = viridis::viridis(n = 3, begin = .95, end = 0))+
   scale_x_date(date_labels = "%b") +
-  theme(text = element_text(size = 15), 
+  theme(text = element_text(size = 20), 
         legend.title=element_text(size=12),
         legend.text=element_text(size=12),
         legend.position = "top")+
